6f2e7a0d1dc84b7f9149251b8ad8426d348d4df4
Candela Capra <mcandela.capra@gmail.com>
2022-02-10 16:33:24 -0300

    Creating Counter

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,15 @@
+import { ItemCount } from '../itemCount/itemCount.js';
+
 const ItemListContainer = ({greeting}) =>{
+    const addToCart = (item) => {
+        console.log(item)
+    }
+
     return (
-        <p className="fs-1 text-center mt-3">{greeting}</p>
+        <>
+            <p className="fs-1 text-center mt-3">{greeting}</p>
+            <ItemCount stock="4" initial="1" onAdd={addToCart}></ItemCount>
+        </>
     )
 }
 
---
+++ src/components/itemCount/itemCount.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import {InputGroup, FormControl, Button, Card} from 'react-bootstrap';
+import {Plus, Dash} from 'react-bootstrap-icons'
+
+function ItemCount ({stock=1, initial=1, onAdd}) {
+    const [number, setNumber] = useState (parseInt(initial)); 
+
+    function add(){
+        if (number<stock){
+            setNumber (number+1);
+        }else{
+            alert("No hay stock")
+        }
+    }
+
+    function subtract(){
+        if (number>initial){
+            setNumber (number-1);
+        }
+    }
+
+    return (
+    <div>
+        <Card style={{ width: '18rem' }} className="text-center mx-auto">
+            <Card.Body>
+                <InputGroup className="mb-3">
+                    <Button variant="info" onClick={subtract}> 
+                        <Dash className="fs-3"></Dash>
+                    </Button>
+                    <FormControl value={number} className="text-center border-info" readOnly/>
+                    <Button variant="info" onClick={add}>
+                        <Plus className="fs-3"></Plus>
+                    </Button>
+                </InputGroup>
+                <Button variant="info" onClick={()=> {onAdd(number)}}>Agregar al carrito</Button>
+            </Card.Body>
+        </Card>    
+    </div>
+    )
+}
+
+export {ItemCount};
+
+//Revisar el tema de las dependencias 
